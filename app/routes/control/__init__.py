34d92e52c005cfbe7825bdeb8fe6eac215233db0
Elvin Guliev <guliev_eg@mail.ru>
2025-12-01 10:30:21 +0300

    :tada: Next commit

--- app/routes/control/__init__.py
+++ app/routes/control/__init__.py
@@ -1,14 +1,14 @@
 from flask import Blueprint
 
-from .dashboard import dashboard_bp
 from .employees import employees_bp
+from .home import home_bp
 from .orders import orders_bp
 from .users import users_bp
 from .works import works_bp
 
 control_bp: Blueprint = Blueprint("control", __name__, url_prefix="/control")
 
-control_bp.register_blueprint(dashboard_bp)
+control_bp.register_blueprint(home_bp)
 control_bp.register_blueprint(orders_bp)
 control_bp.register_blueprint(users_bp)
 control_bp.register_blueprint(employees_bp)
--- app/routes/control/home.py
+++ app/routes/control/home.py
@@ -6,18 +6,18 @@ from flask_login import current_user, login_required
 from app.db import DatabaseManager
 from app.utils import permission_required
 
-dashboard_bp: Blueprint = Blueprint("dashboard", __name__)
+home_bp: Blueprint = Blueprint("home", __name__)
 db_manager: DatabaseManager = DatabaseManager()
 
 
-@dashboard_bp.route("/overview", methods=["GET"])
+@home_bp.route("/index", methods=["GET"])
 @login_required
 @permission_required(["advanced"])
-def overview() -> str:
+def index() -> str:
     context: Dict[str, int] = {
         "user_name": current_user.name,
         "total_orders": db_manager.orders.get_total_order_count(),
         "total_employees": db_manager.employees.get_total_employee_count(),
         "total_tasks": db_manager.tasks.get_total_task_count(),
     }
-    return render_template("control/dashboard/overview.html", **context)
+    return render_template("control/home/index.html", **context)
