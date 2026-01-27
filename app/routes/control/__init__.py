391a89d7780b3a0ab778888b933e7eb4a280e0aa
Elvin Guliev <guliev_eg@mail.ru>
2026-01-27 10:30:46 +0300

    :tada: Next commit

--- app/routes/control/__init__.py
+++ app/routes/control/__init__.py
@@ -17,6 +17,7 @@ from .works import works_bp
 control_bp: Blueprint = Blueprint("control", __name__, url_prefix="/control")
 
 control_bp.register_blueprint(employees_bp)
+control_bp.register_blueprint(hours_bp)
 control_bp.register_blueprint(logs_bp)
 control_bp.register_blueprint(orders_bp)
 control_bp.register_blueprint(tasks_bp)
