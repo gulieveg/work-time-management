7e4e4b6e8e2e66f4e52cd8f1e186356b90fd9c99
Elvin Guliev <guliev_eg@mail.ru>
2026-01-29 15:48:55 +0300

    :tada: Next commit

--- app/routes/control/reports.py
+++ app/routes/control/reports.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 from app.db import DatabaseManager
@@ -11,6 +11,7 @@ db_manager: DatabaseManager = DatabaseManager()
 @reports_bp.route("", methods=["GET"])
 @login_required
 @permission_required(["advanced"])
-def reports() -> str:
+def generate_reports() -> str:
     start_date: str = request.form.get("start_date")
     end_date: str = request.form.get("end_date")
+    return render_template("control/reports/generate_reports.html")
