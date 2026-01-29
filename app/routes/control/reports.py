5cede3e25a47292a8c16715286f06bd339ce170b
Elvin Guliev <guliev_eg@mail.ru>
2026-01-29 15:03:57 +0300

    :tada: Next commit

--- app/routes/control/reports.py
+++ app/routes/control/reports.py
@@ -1,3 +1,6 @@
 from flask import Blueprint
 
+from app.db import DatabaseManager
+
 reports_bp: Blueprint = Blueprint("reports", __name__, url_prefix="/reports")
+db_manager: DatabaseManager = DatabaseManager()
