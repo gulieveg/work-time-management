6f9e3cbeb24a1c2f10c5794dccb9338645dae544
Elvin Guliev <guliev_eg@mail.ru>
2026-01-29 16:00:01 +0300

    :tada: Next commit

--- app/routes/control/reports.py
+++ app/routes/control/reports.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from typing import Dict, List, Union
+
 from flask import Blueprint, render_template, request
 from flask_login import login_required
 
@@ -8,10 +11,20 @@ reports_bp: Blueprint = Blueprint("reports", __name__, url_prefix="/reports")
 db_manager: DatabaseManager = DatabaseManager()
 
 
+Tasks = List[Dict[str, Union[str, Decimal]]]
+
+
 @reports_bp.route("", methods=["GET"])
 @login_required
 @permission_required(["advanced"])
 def generate_reports() -> str:
     start_date: str = request.form.get("start_date")
     end_date: str = request.form.get("end_date")
+
+    args: Dict[str, str] = {
+        "start_date": request.args.get("start_date"),
+        "end_date": request.args.get("end_date"),
+    }
+
+    tasks: Tasks = db_manager.tasks.get_tasks(**args)
     return render_template("control/reports/generate_reports.html")
