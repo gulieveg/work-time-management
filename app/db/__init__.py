b383388543ca149816459f4df07461fbd62a16b5
Elvin Guliev <guliev_eg@mail.ru>
2026-01-30 09:43:32 +0300

    :tada: Next commit

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,1 +1,5 @@
 from .db_manager import DatabaseManager
+
+
+class DatabaseUnavailable(Exception):
+    pass
---
+++ app/utils/errors.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from werkzeug.exceptions import NotFound, ServiceUnavailable
+
+from app.db import DatabaseUnavailable
+
+
+def handle_error_404(app: Flask) -> callable:
+    @app.errorhandler(404)
+    def page_not_found(error: NotFound) -> str:
+        return render_template("errors/404.html"), 404
+
+    return page_not_found
+
+
+def handle_error_503(app: Flask) -> callable:
+    @app.errorhandler(DatabaseUnavailable)
+    def service_unavailable(error: ServiceUnavailable) -> str:
+        return render_template("errors/503.html"), 503
+
+    return service_unavailable
