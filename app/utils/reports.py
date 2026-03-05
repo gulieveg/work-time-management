197ab78c70439d5cfab6cfbbfc3699be9467cbb0
Elvin Guliev <guliev_eg@mail.ru>
2026-03-05 14:29:53 +0300

    :tada: Next commit

--- app/utils/reports.py
+++ app/utils/reports.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
+from datetime import datetime
 from decimal import Decimal
 from io import BytesIO
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 from openpyxl import Workbook
 from openpyxl.styles import Alignment, Border, Font, Side
@@ -8,7 +10,242 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.worksheet.worksheet import Worksheet
 from pandas import DataFrame
 
+Tasks = List[Dict[str, Union[str, Decimal]]]
 Data = List[List[Union[str, Decimal]]]
+from app.db import DatabaseManager
+
+db_manager: DatabaseManager = DatabaseManager()
+
+
+def period_contains_2025(start_date: datetime, end_date: datetime) -> bool:
+    """
+    Checks if the given period contains any date in the year 2025.
+
+    Args:
+        start_date (datetime, optional): The start of the period.
+        end_date (datetime, optional): The end of the period.
+
+    Returns:
+        bool: True if the period includes any date in 2025, False otherwise.
+    """
+    lower_bound: datetime = datetime(2024, 12, 31)
+    upper_bound: datetime = datetime(2026, 1, 1)
+
+    if not start_date and not end_date:
+        return True
+    elif not start_date and end_date and end_date > lower_bound:
+        return True
+    elif start_date and end_date and lower_bound < start_date < upper_bound and end_date >= start_date:
+        return True
+    elif start_date and lower_bound < start_date < upper_bound and not end_date:
+        return True
+    return False
+
+
+def get_tasks_data(tasks: Tasks) -> Data:
+    """
+    Converts tasks object into list of lists for report generation.
+
+    Args:
+        tasks (Tasks): List of task records, each containing employee details,
+            order information, and work metrics.
+
+    Returns:
+        tasks_data (Data): List of lists, where each inner list contains the data for one specific task.
+    """
+
+    employee_categories: Dict[str, str] = {
+        "worker": "Рабочий",
+        "specialist": "Специалист",
+        "manager": "Руководитель",
+    }
+
+    tasks_data: Data = [
+        [
+            task["employee_name"],
+            task["personnel_number"],
+            employee_categories[task["employee_category"]],
+            task["department"],
+            task["order_number"],
+            task["order_name"],
+            task["work_name"],
+            task["hours"],
+            task["operation_date"],
+        ]
+        for task in tasks
+    ]
+    return tasks_data
+
+
+def get_employees_data(tasks: Tasks) -> Data:
+    """
+    Groups tasks by employee and date, aggregating hours for report generation.
+
+    Creates unique key from employee details and date to aggregate hours,
+    then formats the result with translated category names.
+
+    Args:
+        tasks (Tasks): List of task records, each containing employee details,
+            order information, and work metrics.
+
+    Returns:
+        employees_data (Data):  List of lists, where each inner list contains the data for one specific employee
+            on specific date.
+    """
+
+    spent_hours_per_employee: Dict[Tuple[str, ...], Decimal] = defaultdict(Decimal)
+
+    for task in tasks:
+        key: Tuple[str, ...] = (
+            task["employee_name"],
+            task["personnel_number"],
+            task["employee_category"],
+            task["department"],
+            task["operation_date"],
+        )
+        spent_hours_per_employee[key] += task["hours"]
+
+    employee_categories: Dict[str, str] = {
+        "worker": "Рабочий",
+        "specialist": "Специалист",
+        "manager": "Руководитель",
+    }
+
+    employees_data: Data = [
+        [
+            employee_details[0],
+            employee_details[1],
+            employee_categories[employee_details[2]],
+            employee_details[3],
+            operation_date,
+            spent_hours,
+        ]
+        for (*employee_details, operation_date), spent_hours in spent_hours_per_employee.items()
+    ]
+    return employees_data
+
+
+def get_basic_orders_data(tasks: Tasks, start_date: datetime, end_date: datetime) -> Data:
+    """
+    Returns orders data including planned, spent, and remaining hours.
+
+    This function calculates the total spent hours per order based on the provided tasks,
+    optionally includes spent hours for the year 2025, if the specified date range requires it.
+
+    Args:
+        tasks (Tasks): List of task records, each containing employee details,
+            order information, and work metrics.
+        start_date (datetime): The start date for selecting tasks from the database.
+        end_date (datetime): The end date for selecting tasks from the database.
+
+    Returns:
+        orders_data (Data): List of lists, where each inner list contains the data for one specific order,
+            including its number, name, planned hours, spent hours, and remaining hours.
+            The final inner list in the outer list contains the totals for planned hours, spent hours,
+            and remaining hours calculated across all orders in the dataset.
+    """
+
+    spent_hours_per_order: Dict[str, Decimal] = defaultdict(Decimal)
+
+    for task in tasks:
+        spent_hours_per_order[task["order_number"]] += task["hours"]
+
+    if period_contains_2025(start_date=start_date, end_date=end_date):
+        spent_hours_for_2025: Dict[str, Decimal] = db_manager.orders.get_spent_hours_for_2025()
+
+        for order_number, spent_hours in spent_hours_for_2025.items():
+            spent_hours_per_order[order_number] += spent_hours
+
+    orders_data: Data = []
+
+    order_numbers: Tuple[str] = tuple(spent_hours_per_order.keys())
+
+    if order_numbers:
+        planned_hours_per_order: List = db_manager.orders.get_planned_hours_per_order(order_numbers=order_numbers)
+
+        for order_number, order_name, planned_hours in planned_hours_per_order:
+            spent_hours: Decimal = spent_hours_per_order[order_number]
+            remaining_hours: Decimal = planned_hours - spent_hours
+            orders_data.append(
+                [
+                    order_number,
+                    order_name,
+                    planned_hours,
+                    spent_hours,
+                    remaining_hours,
+                ]
+            )
+
+    planned_hours, spent_hours, remaining_hours = Decimal(0), Decimal(0), Decimal(0)
+
+    for order_data in orders_data:
+        planned_hours += order_data[2]
+        spent_hours += order_data[3]
+        remaining_hours += order_data[4]
+
+    orders_data.append(["Итого", "", planned_hours, spent_hours, remaining_hours])
+    return orders_data
+
+
+def get_detailed_orders_data(tasks: Tasks) -> Data:
+    """
+    Returns order data with detailed information by types of work.
+
+    This function extends the basic orders data by providing information on work types
+    associated with each order. For each order it displays list of work types with their planned,
+    spent and remaining hours.
+
+    The function calculates spent hours of work types from the provided tasks for each order and
+    displays all associated work types with their corresponding planned, spent and remaining hours.
+
+    This provides detailed view of hour distribution across different work types within each order.
+
+    Args:
+        tasks (Tasks): List of task records, each containing employee details,
+            order information, and work metrics.
+
+    Returns:
+        orders_data (Data): List of lists, where each inner list contains the data for one specific order,
+            including its number, name, work name, planned hours, spent hours, and remaining hours.
+    """
+
+    spent_hours_per_work: Dict[str, Decimal] = defaultdict(Decimal)
+
+    for task in tasks:
+        key: Tuple[str, str] = (
+            task["order_number"],
+            task["work_name"],
+        )
+        spent_hours_per_work[key] += task["hours"]
+
+    order_numbers, work_names = [], []
+
+    for order_number, work_name in spent_hours_per_work.keys():
+        order_numbers.append(order_number)
+        work_names.append(work_name)
+
+    orders_data: Data = []
+
+    if order_numbers and work_names:
+        planned_hours_per_work: List = db_manager.works.get_planned_hours_per_work(
+            order_numbers=order_numbers,
+            work_names=work_names,
+        )
+
+        for order_number, order_name, work_name, planned_hours in planned_hours_per_work:
+            spent_hours: Decimal = spent_hours_per_work[(order_number, work_name)]
+            remaining_hours: Decimal = planned_hours - spent_hours
+            orders_data.append(
+                [
+                    order_number,
+                    order_name,
+                    work_name,
+                    planned_hours,
+                    spent_hours,
+                    remaining_hours,
+                ]
+            )
+        return orders_data
 
 
 def configure_worksheet_columns(
