ec2312059cdf7a4514d77f73ee50774625c256e0
Elvin Guliev <guliev_eg@mail.ru>
2026-03-05 14:31:09 +0300

    :tada: Next commit

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,7 +3,13 @@ from flask import Flask
 from .errors import handle_error_404
 from .messages import MESSAGES
 from .permissions import permission_required
-from .reports import get_report_file
+from .reports import (
+    get_basic_orders_data,
+    get_detailed_orders_data,
+    get_employees_data,
+    get_report_file,
+    get_tasks_data,
+)
 from .template_filters import zip_iterables
 
 
